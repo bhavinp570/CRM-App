c5313c945ea99597ebb08c640a7ba54a6becc2d5
Bhavin Patel <39761550+bhavinp570@users.noreply.github.com>
2018-05-31 11:50:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head> 
+<head>
     <title>Invoice </title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
